06ff5fc9783dc83f244ec6d25ecb5a7ae50f261a
memories2013 <nshivaprasad41@gmail.com>
2021-11-10 16:48:25 +0530

    InitialCommit

---
+++ src/com/core/TestClass.java
@@ -0,0 +1,12 @@
+package com.core;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Largest obj = new Largest();
+		obj.largest();
+	}
+
+
+}
