e90cf2547d717c6dbafb84fc551461779fe36c3a
memories2013 <nshivaprasad41@gmail.com>
2021-11-10 17:05:04 +0530

    InitialCommit

---
+++ src/com/functional/TestFunction.java
@@ -0,0 +1,12 @@
+package com.functional;
+
+public class TestFunction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TwoDArray obj = new TwoDArray();
+		obj.start();
+	}
+
+
+}
